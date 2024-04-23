a70f40d7d04e19451d8a408e9c42d954775af0fb
banu-drylogics <banupriya@drylogics.com>
2024-04-23 12:37:18 +0530

    Remove any type

--- src/programs/example.tsx
+++ src/programs/example.tsx
@@ -3,7 +3,16 @@ const _ = require('lodash');
 
 
 interface ConditionProps {
-    [index: number]: any | any[];
+    [index: number]: string | string[] | ConditionProps[];
+}
+
+interface JsonItemsProps {
+    first_name: string;
+    last_name: string;
+    email: string;
+    gender: string;
+    country: string;
+    [key: string]: string; // Index signature to allow any additional properties
 }
 
 const isOperator = (val: string) => ['not', 'or', 'and'].includes(val);
@@ -20,54 +29,56 @@ const filterCriterias = (conditions: ConditionProps) => {
     const criterias: ConditionProps[] = [];
     _.forEach(conditions, (condition: ConditionProps[]) => {
         if (Array.isArray(condition) || !isOperator(condition)) {
-            criterias.push(condition);
+            criterias.push(condition as string[]);
         }
     })
     return criterias;
 }
 
-const hasCondition = (item: any, criteria: ConditionProps) => {
+const updateKey = (key: string): string => key === 'from_country' ? 'country' : 'gender_is' ? 'gender' : key;
+
+const checkCondition = (item: JsonItemsProps, criteria: ConditionProps): any => {
     const hasOperator = filterOperator(criteria);
     if (hasOperator) {
-        const nextCri = filterCriterias(criteria);
-        return applyCondition(item, hasOperator, nextCri)
+        const nextCriteria = filterCriterias(criteria);
+        return applyCondition(item, hasOperator, nextCriteria as string[])
     }
     const key = criteria[0];
     const values = criteria[1];
-    const nKey = key === 'from_country' ? 'country' : 'gender_is' ? 'gender' : key;
+    const nKey = updateKey(key as string)
     if (Array.isArray(values)) {
         return values.includes(item[nKey])
     }
     else {
-        return item[nKey] === values
+        return values === item[nKey]
     }
 };
 
-const applyCondition = (item: any, operator: string, criterias: ConditionProps[]): boolean => {
+const applyCondition = (item: JsonItemsProps, operator: string | undefined, criterias: ConditionProps): boolean => {
     switch (operator) {
         case 'and':
-            return criterias.every((criteria: any) => hasCondition(item, criteria));
+            return _.every(criterias, (criteria: ConditionProps) => checkCondition(item, criteria));
         case 'or':
-            return criterias.some((criteria: any) => hasCondition(item, criteria));
+            return _.some(criterias, (criteria: ConditionProps) => checkCondition(item, criteria));
         case 'not':
-            return criterias.every((criteria: any) => !hasCondition(item, criteria));
+            return _.every(criterias, (criteria: ConditionProps) => !checkCondition(item, criteria));
         default:
-            return hasCondition(item, criterias)
+            return checkCondition(item, criterias)
     }
 }
 
 
-export const filterData = (jsonVal: any, conditions: ConditionProps | ConditionProps[]) => {
-    return jsonVal.filter((data: any) => {
+export const filterData = (jsonVal: JsonItemsProps[], conditions: ConditionProps) => {
+    return _.filter(jsonVal, (data: JsonItemsProps) => {
         const operator = filterOperator(conditions);
         const criterias = filterCriterias(conditions);
-        return applyCondition(data, operator, criterias);
+        return applyCondition(data, operator, criterias as string[]);
     });
 }
 // console.log(filterData(jsonVal, ['from_country', 'India']))
 // console.log(filterData(jsonVal, ['from_country', ['India', 'Russia']]))
 // console.log(filterData(jsonVal, ['gender_is', ['Male']]))
 // console.log(filterData(jsonVal, ['not', ['from_country', ['France', 'China']]]))
-console.log(filterData(jsonVal, ['or', ['from_country', ['Russia']], ['gender_is', ['Male']]]))
-// console.log(filterData(jsonVal, ['and', ['from_country', ['Russia', 'Mexico']], ['not', ['gender_is', 'Male']]]))
+// console.log(filterData(jsonVal, ['or', ['from_country', ['Russia']], ['gender_is', ['Male']]]))
+console.log(filterData(jsonVal, ['and', ['from_country', ['Russia', 'Mexico']], ['not', ['gender_is', 'Male']]]))
 
