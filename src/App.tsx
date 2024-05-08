528b9c85ae158d7a7391b040ab64df0632e4bf36
banu-drylogics <banupriya@drylogics.com>
2024-05-08 19:18:52 +0530

    Text box parent and child node created test

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import React, { useState } from 'react';
 import './App.css';
-import ChildrenCharacterView from './components/children-character/ChildrenCharacterView';
+import TextBoxComponent from './components/children-character/TextBoxComponent';
 
 
 function App() {
 
   return (
     <div className="container">
-      <div className='children-container'>
-        <ChildrenCharacterView />
-      </div>
+      <TextBoxComponent />
     </div>
   );
 }
